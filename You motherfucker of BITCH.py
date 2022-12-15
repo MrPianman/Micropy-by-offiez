e6adda171d7c507c71a637d8b72d231f63b88492
Jpian <101275229+MrPianman@users.noreply.github.com>
2022-12-15 09:42:52 +0700

    Update You motherfucker of BITCH.py

--- You motherfucker of BITCH.py
+++ You motherfucker of BITCH.py
@@ -5,7 +5,7 @@ p = PWM(Pin(25))
 #p.freq(100)
 sw26 = Pin(26,Pin.IN)
 led18 = Pin(19,Pin.OUT)
-spd=0.7
+spd=1
 p.duty(100)
 
 def light():
@@ -15,22 +15,30 @@ def light():
         led18.value(0)
         s(0.5)
 
-def speed(): pass
-
+def speed():
+    if sw26.value() and spd==1:
+        spd=0.7
+        sleep(0.5)
+    elif sw26.value() and spd==0.7:
+        spd=0.5
+        sleep(0.5)
+    elif sw26.value() and spd==0.5:
+        spd=1
+        sleep(0.5)
+        
 def f(a):
     p.freq(a)
-    speed()
 
 def s(b):
     sleep(b)
-    speed()
 
 def pa(c):
     p.freq(25000)
     sleep(c)
 
+sw26.irq(trigger=Pin.IRQ_FALLING,handler=speed)
 
-def song2(spd):
+while True:
     C4=261; CS4=277; D4=293; DS4=311; E4=329; F4=349; FS4=370; G4=392; GS4=415; A4=440; AS4=466; B4=493
     C5=523; CS5=554; D5=587; DS5=622; E5=659; F5=698; FS5=734; G5=784; GS5=830; A5=880; AS5=932; B5=987
     C6=1046; CS6=1108; D6=1174; DS6=1244; E6=1318; F6=1396; FS6=1480; G6=1568; GS6=1661; A6=1760; AS6=1864; B6=1975
@@ -159,24 +167,11 @@ def song2(spd):
     print(7)
     print(spd)
 
-def song():
-    while sw26.value():
-        song2(1)
-    while sw26.value():
-        song2(0.7)
-    while sw26.value():
-        song2(0.5)
         
-
+'''
 C4=261; CS4=277; D4=293; DS4=311; E4=329; F4=349; FS4=370; G4=392; GS4=415; A4=440; AS4=466; B4=493
 C5=523; CS5=554; D5=587; DS5=622; E5=659; F5=698; FS5=734; G5=784; GS5=830; A5=880; AS5=932; B5=987
 C6=1046; CS6=1108; D6=1174; DS6=1244; E6=1318; F6=1396; FS6=1480; G6=1568; GS6=1661; A6=1760; AS6=1864; B6=1975
+'''
 
-thread1 = threading.Thread(target=light)
-thread2 = threading.Thread(target=song)
-
-thread1.start()
-thread2.start()
-
-
-#stackoverflow help me
+#stackoverflow help me 
